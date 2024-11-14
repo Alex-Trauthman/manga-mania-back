7aeffbd45aa41fc811137c32c2c8abbeca8d2ddb
SlonyRufus <33912376+slonyrufus@users.noreply.github.com>
2024-11-14 03:15:03 -0300

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,13 +3,51 @@ INSERT INTO Administrador (username, email, senha, cpf) VALUES ('adminadmin', 'a
 INSERT INTO AutorManga (nome, anoNascimento, nacionalidade, sexo) VALUES ('Autor 1', 1970, 'Japão', '2');
 INSERT INTO AutorManga (nome, anoNascimento, nacionalidade, sexo) VALUES ('Autor 2', 1980, 'Japão', '1');
 
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 0', 1965, 'Japão', '1');
 INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 1', 1965, 'Japão', '1');
 INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 2', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 3', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 4', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 5', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 6', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 7', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 8', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 9', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 10', 1965, 'Japão', '1');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 11', 1965, 'Japão', '1');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 12', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 13', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 14', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 15', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 16', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 17', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 18', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 19', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 20', 1965, 'Japão', '1');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 21', 1965, 'Japão', '1');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 22', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 23', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 24', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 25', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 26', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 27', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 28', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 29', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 30', 1965, 'Japão', '1');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 31', 1965, 'Japão', '1');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 32', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 33', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 34', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 35', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 36', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 37', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 38', 1975, 'Japão', '2');
+INSERT INTO EscritorNovel (nome, anoNascimento, nacionalidade, sexo) VALUES ('Escritor 39', 1975, 'Japão', '2');
 
-INSERT INTO Manga (nome, nomeImagem, paginas, preco, sinopse, anoPublicacao, estoque, color, generoManga, autorManga_id) VALUES ('Manga 1', "aaaa", 2000, 200, 19.99, 'Sinopse do Manga 1', 50, "CO", 1, 1);
-INSERT INTO Manga (nome, nomeImagem, paginas, preco, sinopse, anoPublicacao, estoque, color, generoManga, autorManga_id) VALUES ('Manga 2', "aaaa", 2005, 250, 24.99, 'Sinopse do Manga 2', 30, "CO", 2, 2);
+INSERT INTO Manga (nome, nomeImagem, paginas, preco, sinopse, anoPublicacao, estoque, colorido, generoManga, autorManga_id) VALUES ('Manga 1', 'aaaa', 200, 19.99, 'Sinopse do Manga 1', 1950, 50, 'CO', 1, 1);
+INSERT INTO Manga (nome, nomeImagem, paginas, preco, sinopse, anoPublicacao, estoque, colorido, generoManga, autorManga_id) VALUES ('Manga 2', 'aaaa', 250, 24.99, 'Sinopse do Manga 2', 2005, 30, 'CO', 2, 2);
 
-INSERT INTO Novel (nome, nomeImagem, paginas, preco, sinopse, anoPublicacao, estoque, genero, capitulos, escritorNovel_id) VALUES ('Novel 1', "aaaa", 1995, 300, 29.99, 'Sinopse do Novel 1', 20, '1', 20, 1);
-INSERT INTO Novel (nome, nomeImagem, paginas, preco, sinopse, anoPublicacao, estoque, genero, capitulos, escritorNovel_id) VALUES ('Novel 2', "aaaa", 2000, 350, 34.99, 'Sinopse do Novel 2', 15, '4', 30, 2);
+INSERT INTO Novel (nome, nomeImagem, paginas, preco, sinopse, anoPublicacao, estoque, genero, capitulos, escritorNovel_id) VALUES ('Novel 1', 'aaaa', 300, 29.99, 'Sinopse do Novel 1', 1995, 20, '1', 20, 1);
+INSERT INTO Novel (nome, nomeImagem, paginas, preco, sinopse, anoPublicacao, estoque, genero, capitulos, escritorNovel_id) VALUES ('Novel 2', 'aaaa', 350, 34.99, 'Sinopse do Novel 2', 1945, 15, '4', 30, 2);
 
 INSERT INTO Usuario (username, email, senha, cpf, endereco,sexo) VALUES ('usuariousuario', 'usuariousuario@email.com', 'jToWaGeHuPUor3r9CkTLqX0iYcmTwVAWXyxdJL5VrqG1m/6Kx2iImpZq3i65bJQsLPztfaHi3q/rqDj1QTbqHA==', '12345678900', 'lo4 alameda 2',1); -- 123456789123456789
